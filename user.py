9339feb0a38e2b3ed01a72df23efa90a80857580
toto <Samuel CHICHA>
2023-03-14 14:17:20 +0000

    build: user information script using id command

---
+++ user.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import sys
+import subprocess
+import os
+import re
+import getpass
+
+if __name__ == "__main__":
+    username = input("Type your username: ")
+    id_result = subprocess.run(["id", f"{username}"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if id_result.returncode != 0:
+        print(id_result.stderr.decode())
+        exit(1)
+    match = re.match(r"uid=(?P<uid>\d+)\(\w+\) gid=.* groups=(?P<groups>\d+\(\w+\)(,\d+\((\w+)\))*)", id_result.stdout.decode())
+    if match is None:
+        print("Could not find information about the specified user.")
+        print(id_result.stdout.decode())
+        exit(3)
+    uid = match.group('uid')
+    print(f"Username: {username}")
+    print(f"User id: {uid}")
+    groups = re.findall(r"\((\w+)\)", match.group('groups'))
+    print(f"Groups: {', '.join(groups)}")
+    path = input("Where do you want to store the information ?")
+    try:
+        with open(path, "w") as file:
+            file.write(f"username {username}\n")
+            file.write(f"user id {uid}\n")
+            file.write(f"groups: {', '.join(groups)}\n")
+    except FileNotFoundError as fnfe:
+        print(fnfe)
+        exit(2)
+    exit(0)
