9bf7fba81d515404743adf48403d59225516bd9e
toto <Samuel CHICHA>
2023-03-21 13:49:45 +0000

    build: rework user.py + user.bash

--- user.bash
+++ user.bash
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 
-echo -n "Type your username: "
-read -r username
+username=$USER
 if ! id_cmd=$(id $username 2>&1)
 then
     echo "Failure: $id_cmd"
@@ -24,11 +23,20 @@ then
 fi
 echo "User name: $username"
 echo "User id: $uid"
-# echo "Groups: $groups"
 echo "Groups:"
 echo "$group_names"
-echo -n "Where do you want to store the information ?"
-read -r path
+path="user_information.txt"
+i=0
+while [[ $i -lt 100 && -f "$path" ]]
+do
+    i=$((i + 1))
+    path="user_information($i).txt"
+done
+if [[ $i -eq 100 ]]
+then
+    echo "Could not store the information in a new file"
+    exit 3
+fi
 echo "User name: $username" > "$path"
 echo "User id: $uid" >> "$path"
 echo "Groups: $group_names" >> "$path"
--- user.py
+++ user.py
@@ -1,34 +1,31 @@
 #!/usr/bin/python3
 
-import sys
-import subprocess
 import os
-import re
 import getpass
+import grp
 
 if __name__ == "__main__":
-    username = input("Type your username: ")
-    id_result = subprocess.run(["id", f"{username}"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    if id_result.returncode != 0:
-        print(id_result.stderr.decode())
-        exit(1)
-    match = re.match(r"uid=(?P<uid>\d+)\(\w+\) gid=.* groups=(?P<groups>\d+\(\w+\)(,\d+\((\w+)\))*)", id_result.stdout.decode())
-    if match is None:
-        print("Could not find information about the specified user.")
-        print(id_result.stdout.decode())
-        exit(3)
-    uid = match.group('uid')
+    username = getpass.getuser()
+    uid = os.getuid()
+    groups_ids = os.getgroups()
     print(f"User name: {username}")
     print(f"User id: {uid}")
-    groups = re.findall(r"\((\w+)\)", match.group('groups'))
+    groups = [group.gr_name for group in grp.getgrall() if group.gr_gid in groups_ids]
     print(f"Groups: {', '.join(groups)}")
-    path = input("Where do you want to store the information ?")
+    path = "user_information.txt"
+    i = 0
+    while i < 100 and os.path.exists(path):
+        i += 1
+        path = f"user_information({i}).txt"
+    if i == 100:
+        print("Could not store the information in a new file")
+        exit(1)
     try:
         with open(path, "w") as file:
             file.write(f"User name: {username}\n")
             file.write(f"User id: {uid}\n")
             file.write(f"Groups: {', '.join(groups)}\n")
-    except FileNotFoundError as fnfe:
-        print(fnfe)
+    except Exception as e:
+        print(e)
         exit(2)
     exit(0)
